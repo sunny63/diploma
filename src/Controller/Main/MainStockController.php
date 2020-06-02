9b5c1efbec8ddbaeee3a7bfaf9918ebfe0a16893
Natalia Bykova <hataiiia1999@mail.ru>
2020-06-02 21:59:47 +1000

    add stock viewing page

--- src/Controller/Main/MainStockController.php
+++ src/Controller/Main/MainStockController.php
@@ -1,10 +1,12 @@
 <?php
 
+
 namespace App\Controller\Main;
 
 
+use App\Entity\Category;
+use App\Entity\Post;
 use App\Entity\Stock;
-use App\Form\StockType;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -18,22 +20,11 @@ class MainStockController extends BaseController
     public function index(int $id, Request $request)
     {
         $stock = $this->getDoctrine()->getRepository(Stock::class)->find($id);
-        $form = $this->createForm(StockType::class, $stock);
-        $em = $this->getDoctrine()->getManager();
-        $form->handleRequest($request);
-
-        $date = new \DateTime('now');
-
-        $categoriesNow = $this->getDoctrine()
-            ->getRepository(Stock::class)
-            ->findAllPastStock($date);
 
         $forRender = parent::renderDefault();
         $forRender['h1'] = 'Новости';
-//        $forRender['stocks'] = $stocks;
-//        $forRender['posts'] = $posts;
-//        $forRender['categories'] = $categories;
-        $forRender['categoriesNow'] = $categoriesNow;
-        return $this->render("main/index.html.twig", $forRender);
+        $forRender['stock'] = $stock;
+//        $forRender['categoriesNow'] = $categoriesNow;
+        return $this->render("main/stock/index.html.twig", $forRender);
     }
 }
